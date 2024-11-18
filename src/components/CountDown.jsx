4ada6a34246ba0e8225ec7a568c56029c8d1d3b8
karanyadavv <karanyadav729@gmail.com>
2024-11-18 22:24:02 +0530

    intial state workaround

--- src/components/CountDown.jsx
+++ src/components/CountDown.jsx
@@ -2,10 +2,10 @@ import { useEffect } from "react";
 import { useState } from "react";
 
 export default function CountDown() {
-  const [days, setDays] = useState();
-  const [hours, setHours] = useState();
-  const [minutes, setMinutes] = useState();
-  const [seconds, setSeconds] = useState();
+  const [days, setDays] = useState("00");
+  const [hours, setHours] = useState("00");
+  const [minutes, setMinutes] = useState("00");
+  const [seconds, setSeconds] = useState("00");
 
   let intervalId = "";
   useEffect(() => {
