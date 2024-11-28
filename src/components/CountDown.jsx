2b36cd5e7dfa06b7fd4ac115fc1c8c0a81e51fee
karanyadavv <karanyadav729@gmail.com>
2024-11-28 23:23:08 +0530

    Added specific goal component

--- src/components/CountDown.jsx
+++ src/components/CountDown.jsx
@@ -1,8 +1,13 @@
 import { useEffect } from "react";
 import { useState } from "react";
 import NumberFlow from "@number-flow/react";
+import SpecificGoal from "./SpecificGoal";
 
 export default function CountDown() {
+  const currentYear = new Date().getFullYear() + 1;
+  const initialCountDown = new Date(`1/1/${currentYear}`);
+  const [lastDay, setLastDay] = useState(initialCountDown);
+
   const [time, setTime] = useState({
     days: "00",
     hours: "00",
@@ -14,8 +19,8 @@ export default function CountDown() {
     const fetchGoal = () => {
       const oneDay = 24 * 60 * 60 * 1000;
       let currentDate = new Date();
-      let currentYear = currentDate.getFullYear() + 1;
-      let lastDay = new Date(`1/1/${currentYear}`);
+      // let currentYear = currentDate.getFullYear() + 1;
+      // let lastDay = new Date(`1/1/${currentYear}`);
       let total = lastDay - currentDate;
 
       const daysWithoutZero = Math.round(
@@ -41,7 +46,7 @@ export default function CountDown() {
     };
   }, []);
   return (
-    <div className="bg-black h-screen flex flex-col items-center justify-center font-inter">
+    <div className="bg-black h-screen flex flex-col items-center justify-center font-inter fixed inset-0">
       <div className="flex flex-col items-center justify-center space-y-8 text-white leading-relaxed">
         <div className="text-sm md:text-2xl border border-zinc-600 rounded-xl px-4 py-2">
           Days until your goal
@@ -52,7 +57,11 @@ export default function CountDown() {
           <NumberFlow value={time.minutes} /> :{" "}
           <NumberFlow value={time.seconds} />
         </div>
+        <div className="text-sm md:text-2xl border border-zinc-600 rounded-xl px-4 py-2">
+          {lastDay.toLocaleDateString()}
+        </div>
       </div>
+      <SpecificGoal />
     </div>
   );
 }
---
+++ src/components/SpecificGoal.jsx
@@ -0,0 +1,31 @@
+export default function SpecificGoal() {
+  const months = [1, 2, 3];
+  return (
+    <div className="text-white flex-col justify-center items-center space-y-6 p-4 border border-zinc-600 rounded-md absolute bottom-12 md:right-12">
+      <div className="text-white flex space-x-8 p-4 border border-zinc-800 rounded-md bottom-0">
+        <div>
+          <select
+            name=""
+            id=""
+            className="bg-black border border-zinc-600 rounded-md mr-2"
+          >
+            {months.map((item, index) => {
+              return (
+                <option value={item} key={index}>
+                  {item}
+                </option>
+              );
+            })}
+          </select>
+          month from now
+        </div>
+        <div>Add your date</div>
+      </div>
+      <div>
+        <button className="bg-white text-black p-2 whitespace-nowrap rounded-md text-sm font-medium disabled:pointer-events-none disabled:opacity-50">
+          Set your goal
+        </button>
+      </div>
+    </div>
+  );
+}
